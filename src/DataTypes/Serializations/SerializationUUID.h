9fe21acdf40729a2b4781ce5383ea9ca3ede98be
Anton Popov <pad11rus@gmail.com>
2021-03-02 16:41:28 +0300

    temp commit

--- src/DataTypes/Serializations/SerializationUUID.h
+++ src/DataTypes/Serializations/SerializationUUID.h
@@ -1,21 +1,13 @@
 #pragma once
 
-#include <Common/UInt128.h>
-#include <DataTypes/DataTypeNumberBase.h>
-#include <DataTypes/IDataType.h>
+#include <DataTypes/Serializations/SerializationNumber.h>
 
 namespace DB
 {
 
-class DataTypeUUID final : public DataTypeNumberBase<UInt128>
+class SerializationUUID final : public SerializationNumber<UInt128>
 {
-
 public:
-    const char * getFamilyName() const override { return "UUID"; }
-    TypeIndex getTypeId() const override { return TypeIndex::UUID; }
-
-    bool equals(const IDataType & rhs) const override;
-
     void serializeText(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
     void deserializeText(IColumn & column, ReadBuffer & istr, const FormatSettings &) const override;
     void serializeTextEscaped(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
@@ -26,12 +18,9 @@ public:
     void deserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings &) const override;
     void serializeTextCSV(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
     void deserializeTextCSV(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const override;
+    void serializeProtobuf(const IColumn & column, size_t row_num, ProtobufWriter & protobuf, size_t & value_index) const override;
+    void deserializeProtobuf(IColumn & column, ProtobufReader & protobuf, bool allow_add_row, bool & row_added) const override;
 
-    bool canBeUsedInBitOperations() const override { return true; }
-    bool canBeInsideNullable() const override { return true; }
-    bool canBeInsideLowCardinality() const override { return false; }
-
-    bool canBePromoted() const override { return false; }
 };
 
 }
