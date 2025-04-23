b028a3d2a0baa005e9216d6ec49dbdc7431aae34
Anton Popov <anton@clickhouse.com>
2025-04-23 11:43:07 +0000

    better polygonArea* functions

--- src/Functions/polygonArea.cpp
+++ src/Functions/polygonArea.cpp
@@ -7,6 +7,10 @@
 #include <Columns/ColumnTuple.h>
 #include <Columns/ColumnsNumber.h>
 #include <DataTypes/DataTypesNumber.h>
+#include <boost/geometry/strategy/geographic/area.hpp>
+#include "Common/PODArray_fwd.h"
+#include "Functions/FunctionHelpers.h"
+#include "base/types.h"
 
 #include <memory>
 #include <string>
@@ -22,11 +26,33 @@ namespace ErrorCodes
 namespace
 {
 
-template <typename Point>
+struct CartesianAreaImpl
+{
+    using Point = CartesianPoint;
+    static constexpr size_t num_arguments = 1;
+    static constexpr auto name = "polygonAreaCartesian";
+};
+
+struct SphericalAreaImpl
+{
+    using Point = SphericalPoint;
+    static constexpr size_t num_arguments = 0;
+    static constexpr auto name = "polygonAreaSpherical";
+};
+
+struct GeographicAreaImpl
+{
+    using Point = SphericalPoint;
+    static constexpr size_t num_arguments = 1;
+    static constexpr auto name = "polygonAreaGeographic";
+};
+
+template <typename Impl>
 class FunctionPolygonArea : public IFunction
 {
 public:
-    static inline const char * name;
+    using Point = typename Impl::Point;
+    static inline const char * name = Impl::name;
 
     explicit FunctionPolygonArea() = default;
 
@@ -40,18 +66,34 @@ public:
         return name;
     }
 
+    bool useDefaultImplementationForConstants() const override
+    {
+        return true;
+    }
+
     bool isVariadic() const override
     {
-        return false;
+        return Impl::num_arguments == 0;
     }
 
     size_t getNumberOfArguments() const override
     {
-        return 1;
+        return Impl::num_arguments;
     }
 
-    DataTypePtr getReturnTypeImpl(const DataTypes &) const override
+    DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
+        if constexpr (std::is_same_v<Impl, SphericalAreaImpl>)
+        {
+            if (arguments.empty() || arguments.size() > 2)
+                throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Function {} requires at most two arguments: polygon and optional radius of the sphere", getName());
+
+            if (arguments.size() == 2 && !isNumber(arguments[1].type) && !isColumnConst(*arguments[1].column))
+                throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "The second argument of function {} must be a number, got: {}", getName(), arguments[1].type->getName());
+        }
+
         return std::make_shared<DataTypeFloat64>();
     }
 
@@ -74,39 +116,64 @@ public:
             using Converter = typename TypeConverter::Type;
 
             if constexpr (std::is_same_v<ColumnToPointsConverter<Point>, Converter>)
+            {
                 throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "The argument of function {} must not be Point", getName());
+            }
             else
             {
                 auto geometries = Converter::convert(arguments[0].column->convertToFullColumnIfConst());
 
-                for (size_t i = 0; i < input_rows_count; ++i)
-                    res_data.emplace_back(boost::geometry::area(geometries[i]));
+                if constexpr (std::is_same_v<Impl, GeographicAreaImpl>)
+                {
+                    boost::geometry::strategy::area::geographic<> strategy_geographic;
+                    executeWithStrategy(res_data, geometries, strategy_geographic);
+                    return;
+                }
+
+                if constexpr (std::is_same_v<Impl, SphericalAreaImpl>)
+                {
+                    if (arguments.size() == 2)
+                    {
+                        if (!arguments[1].column->isNumeric())
+                            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                                "The second argument of function {} must be a constant number, got: {}", getName(), arguments[1].column->getName());
+
+                        Float64 radius = arguments[1].column->getFloat64(0);
+                        boost::geometry::strategy::area::spherical<> strategy_spherical(radius);
+                        executeWithStrategy(res_data, geometries, strategy_spherical);
+                        return;
+                    }
+                }
+
+                executeWithoutStrategy(res_data, geometries);
             }
-        }
-        );
+        });
 
         return res_column;
     }
 
-    bool useDefaultImplementationForConstants() const override
+    template <typename Geometries>
+    void executeWithoutStrategy(PaddedPODArray<Float64> & res_data, const Geometries & geometries) const
     {
-        return true;
+        for (const auto & geometry : geometries)
+            res_data.emplace_back(boost::geometry::area(geometry));
+    }
+
+    template <typename Geometries, typename Strategy>
+    void executeWithStrategy(PaddedPODArray<Float64> & res_data, const Geometries & geometries, const Strategy & strategy) const
+    {
+        for (const auto & geometry : geometries)
+            res_data.emplace_back(boost::geometry::area(geometry, strategy));
     }
 };
-
-template <>
-const char * FunctionPolygonArea<CartesianPoint>::name = "polygonAreaCartesian";
-
-template <>
-const char * FunctionPolygonArea<SphericalPoint>::name = "polygonAreaSpherical";
 
 }
 
 REGISTER_FUNCTION(PolygonArea)
 {
-    factory.registerFunction<FunctionPolygonArea<CartesianPoint>>();
-    factory.registerFunction<FunctionPolygonArea<SphericalPoint>>();
+    factory.registerFunction<FunctionPolygonArea<CartesianAreaImpl>>();
+    factory.registerFunction<FunctionPolygonArea<SphericalAreaImpl>>();
+    factory.registerFunction<FunctionPolygonArea<GeographicAreaImpl>>();
 }
-
 
 }
